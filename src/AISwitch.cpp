8ad4af4b428a727ed3e20ecc9ded6ad2272fd323
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-01-23 15:38:20 +0100

    Update AISwitch.cpp

--- src/AISwitch.cpp
+++ src/AISwitch.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cmath>
-#define N 10
+#define N 15
 float x[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 float y[N] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30};
 float a = 0;
